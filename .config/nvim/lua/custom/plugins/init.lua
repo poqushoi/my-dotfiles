b57c04488fbc229af1e6b811bce9eecc084902d5
poqu U59 <poqushoi@gmail.com>
2024-11-12 20:18:47 +0300

    nvim: add flash, remove leap, add nvim-surround

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -4,12 +4,44 @@
 -- See the kickstart.nvim README for more information
 return {
   'mg979/vim-visual-multi',
+
   {
-    'ggandor/leap.nvim',
+    -- https://www.youtube.com/watch?v=eJ3XV-3uoug
+    -- A package for quick navigation
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {
+      jump = {
+        autojump = true,
+      },
+      modes = {
+        char = {
+          jump_labels = false, -- f, t, F, T with labels
+          multi_line = true,
+          highlight = { backdrop = false }, -- Do not dim text
+        },
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n" },           function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
+  },
+
+  {
+    -- A package for quickly surrounding text
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
     config = function()
-      local leap = require 'leap'
-      leap.create_default_mappings()
-      leap.opts.case_sensitive = true
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
     end,
   },
 }
