616e647721aa0f6f260f5e942919783d4aea4322
poqu U59 <poqushoi@gmail.com>
2024-11-13 19:06:44 +0300

    nvim: pyright setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -631,7 +631,10 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
-        --
+
+        pyright = {
+          filetypes = { 'python' },
+        },
 
         lua_ls = {
           -- cmd = {...},
