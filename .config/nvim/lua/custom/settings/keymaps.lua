911fee4467a7f876a0a94fac549a59b3c345905b
poqu U59 <poqushoi@gmail.com>
2024-11-18 09:37:17 +0300

    nvim: add keymaps no config

---
+++ .config/nvim/lua/custom/settings/keymaps.lua
@@ -0,0 +1,8 @@
+-- Toggle line wrap
+-- vim.keymap.set('n', '<C-w>z', '<cmd>set wrap!<cr>', { silent = true, buffer = 5 })
+
+-- Resize with arrows
+-- vim.keymap.set('n', '<C-S-Down>', ':resize +2<CR>', { desc = 'Resize Horizontal Split Down' })
+-- vim.keymap.set('n', '<C-S-Up>', ':resize -2<CR>', { desc = 'Resize Horizontal Split Up' })
+-- vim.keymap.set('n', '<C-S-Left>', ':vertical resize -2<CR>', { desc = 'Resize Vertical Split Down' })
+-- vim.keymap.set('n', '<C-S-Right>', ':vertical resize +2<CR>', { desc = 'Resize Vertical Split Up' })
