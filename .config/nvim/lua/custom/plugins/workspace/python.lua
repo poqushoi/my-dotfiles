58ec788c44732ed25786474c830859388ef17994
poqu U59 <poqushoi@gmail.com>
2025-01-03 17:02:41 +0300

    nvim: remove NotebookPreview

--- .config/nvim/lua/custom/plugins/workspace/python.lua
+++ .config/nvim/lua/custom/plugins/workspace/python.lua
@@ -7,38 +7,6 @@ return {
   },
 
   {
-    'GCBallesteros/NotebookNavigator.nvim',
-    keys = {
-      {
-        ']j',
-        function()
-          require('notebook-navigator').move_cell 'd'
-        end,
-      },
-      {
-        '[j',
-        function()
-          require('notebook-navigator').move_cell 'u'
-        end,
-      },
-      { '<leader>X', "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
-      { '<leader>x', "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
-    },
-    dependencies = {
-      'echasnovski/mini.comment',
-      'hkupty/iron.nvim', -- repl provider
-      -- "akinsho/toggleterm.nvim", -- alternative repl provider
-      -- "benlubas/molten-nvim", -- alternative repl provider
-      'anuvyklack/hydra.nvim',
-    },
-    event = 'VeryLazy',
-    config = function()
-      local nn = require 'notebook-navigator'
-      nn.setup { activate_hydra_keys = '<leader>h' }
-    end,
-  },
-
-  {
     'linux-cultist/venv-selector.nvim',
     dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
     opts = {
