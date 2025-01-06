4d0425daf4ae15471ad47aa4bd80c2113bd6b028
poqu U59 <poqushoi@gmail.com>
2025-01-06 10:14:56 +0300

    nvim: set tabs using 4 spaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -98,6 +98,9 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Set tabs using 4 spaces
+vim.opt.shiftwidth = 4
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
