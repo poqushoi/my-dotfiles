51b235de49af3f66a8c0f7054f92504c8a1a402e
poqu U59 <poqushoi@gmail.com>
2025-02-01 12:34:16 +0300

    nvim: diagnostics jump shortcuts

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -457,6 +457,11 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- Diagnostics
+          map('<leader>dn', vim.diagnostic.goto_next, 'Go to next diagnostic')
+          map('<leader>dp', vim.diagnostic.goto_prev, 'Go to prev diagnostic')
+          map('<leader>dl', '<CMD>Telescope diagnostics<CR>', 'Show diagnostics list in Telescope')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
