1e00820e8525a03c4e0490c7b1066c8cc73da792
poqu U59 <poqushoi@gmail.com>
2025-02-01 12:33:22 +0300

    wezterm: font and theme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,46 @@
 local wezterm = require 'wezterm'
 local config = {}
 
--- config.color_scheme = 'Batman'
+config.color_scheme = 'Dark+'
+-- config.color_scheme = 'ayu'
+
+config.font = wezterm.font_with_fallback {
+  {
+    family = 'CommitMono Nerd Font',
+  },
+  {
+    family = 'JetBrainsMono Nerd Font',
+  },
+  {
+    family = 'LiterationMono Nerd Font',
+  },
+  {
+    family = 'FiraCode Nerd Font',
+  },
+  {
+    family = 'DejaVuSansMono Nerd Font',
+  },
+  {
+    family = 'Hack Nerd Font',
+  },
+}
+
+config.font_size = 11
 
 -- disable bar at the top
-config.window_decorations = "RESIZE" -- "TITLE | RESIZE"
+config.window_decorations = 'RESIZE' -- "TITLE | RESIZE"
 
-config.window_background_opacity = 0.85
+config.window_background_opacity = 1.0 -- 0.85
+
+wezterm.on('toggle-opacity', function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  if not overrides.window_background_opacity then
+    overrides.window_background_opacity = 1
+  else
+    overrides.window_background_opacity = nil
+  end
+  window:set_config_overrides(overrides)
+end)
 
 -- timeout_milliseconds defaults to 1000 and can be omitted
 config.leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1000 }
@@ -27,9 +61,14 @@ config.keys = {
     action = wezterm.action.CloseCurrentPane { confirm = false },
   },
   {
-    key = 'w',
+    key = 'q',
     mods = 'CTRL|SHIFT',
     action = wezterm.action.CloseCurrentTab { confirm = true },
+  },
+  {
+    key = 'o',
+    mods = 'LEADER',
+    action = wezterm.action.EmitEvent 'toggle-opacity',
   },
 }
 
